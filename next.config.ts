feddc3a12f62b9008141ca28c3a449fc31fed468
Anas Abubakar <anasabubakar7000@gmail.com>
2025-12-05 02:43:30 +0100

    swithed from turbopack to webpack

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,14 @@
 import type { NextConfig } from "next";
-const MonacoWebpackPlugin = require("monaco-editor-webpack-plugin");
+// const MonacoWebpackPlugin = require("monaco-editor-webpack-plugin");
 
 const nextConfig: NextConfig = {
   webpack: (config) => {
-    config.plugins.push(
-      new MonacoWebpackPlugin({
-        languages: ["json", "markdown", "css", "typescript", "javascript", "html", "xml", "go", "python", "java", "cpp"],
-        filename: "static/[name].worker.js",
-      })
-    );
+    // config.plugins.push(
+    //   new MonacoWebpackPlugin({
+    //     languages: ["json", "markdown", "css", "typescript", "javascript", "html", "xml", "go", "python", "java", "cpp"],
+    //     filename: "static/[name].worker.js",
+    //   })
+    // );
     return config;
   },
 };
